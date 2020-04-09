71861c9b17965869a777bbeef80ef2d116d5f6d7
Mike Kao <skybow@gmail.com>
2020-04-10 01:52:12 +0800

    ADD US

--- DRIVING.py
+++ DRIVING.py
@@ -1,8 +1,15 @@
 __county__ = input('國家: ')
 __age__ = input('年齡: ')
 __age__ = int(__age__)
-if __county__ == '台灣':
+if __county__ == 'TW':
   if __age__ >= 18:
     print('可以考駕照!')
   else:
     print('不可以考駕照!')
+elif __county__ =='US':
+  if __age__ >= 16:
+    print('可以考駕照!')
+  else:
+    print('不可以考駕照!')
+else:
+	print('??')
